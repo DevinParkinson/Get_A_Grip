2115889b696dd05d4d91ea25a89ada6b5cef02ca
Devin Parkinson <dparky13@icloud.com>
2018-05-01 11:06:44 -0600

    models and controllers

---
+++ app/models/pistol.rb
@@ -0,0 +1,3 @@
+class Pistol < ApplicationRecord
+  belongs_to :user
+end
---
+++ db/migrate/20180501170529_create_pistols.rb
@@ -0,0 +1,15 @@
+class CreatePistols < ActiveRecord::Migration[5.1]
+  def change
+    create_table :pistols do |t|
+      t.string :make
+      t.string :pistol_model
+      t.string :size
+      t.string :gen
+      t.string :caliber
+      t.float :price
+      t.belongs_to :user, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
