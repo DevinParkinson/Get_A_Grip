15687488a94a0b13fa290c2860a1e5d18c41aaef
Devin Parkinson <dparky13@icloud.com>
2018-05-29 12:32:57 -0600

    getting started on gallery

---
+++ client/src/components/Gallery.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class Gallery extends React.Component {
+  <div>
+  Well, Its something...
+  </div>
+}
+
+export default Gallery;
--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Header, Image, Container, Divider, Grid, Dimmer, Card } from 'semantic-ui-react';
 import { Link } from 'react-router-dom';
 import LandImage from '../images/LandImage.jpg';
